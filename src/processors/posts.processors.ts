f6b627fbd3517f05791d19caf4faf9e624e6078b
Ihor_S <ihor.shcherbyna@clockwise.software>
2024-01-19 17:02:50 +0100

    try: process work only with worker

--- src/processors/posts.processors.ts
+++ src/processors/posts.processors.ts
@@ -2,15 +2,25 @@ import { Process, Processor } from '@nestjs/bull';
 import { CreatePostDto } from '../modules/posts/dtos/createPost.dto';
 import { Job } from 'bull';
 import { PostsService } from '../modules/posts/posts.service';
+import { ConfigService } from '@nestjs/config';
 
 @Processor('posts')
 export class PostsProcessor {
-  constructor(private readonly postsService: PostsService) {}
+  constructor(
+    private readonly postsService: PostsService,
+    private readonly confingService: ConfigService,
+  ) {}
   @Process()
   async processPost(job: Job<CreatePostDto>) {
     console.log(`started job processor for job id  ${job.id}`);
-    await new Promise<void>((resolve) => setTimeout(() => resolve(), 8000));
-    const res = await this.postsService.create(job.data);
-    console.log('job ---------DONE-----------: ', res);
+    const isWorker = this.confingService.get('IS_WORKER');
+    if (isWorker) {
+      console.log('worker is running');
+      await new Promise<void>((resolve) => setTimeout(() => resolve(), 8000));
+      const res = await this.postsService.create(job.data);
+      console.log('job ---------DONE-----------: ', res);
+    } else {
+      console.log('worker is not running, keep work for worker');
+    }
   }
 }
